aa225369dde40d2d8a74aae17dd1273344a4b99c
thexovc <osazeepeter79@gmail.com>
2026-07-25 07:20:16 +0100

    ecs agent config

--- terraform/modules/ecs_ec2/templates/user_data.sh
+++ terraform/modules/ecs_ec2/templates/user_data.sh
@@ -11,6 +11,9 @@ fi
 # Configure ECS Agent to register this instance to our ManyChurch cluster
 echo "ECS_CLUSTER=${cluster_name}" >> /etc/ecs/ecs.config
 echo "ECS_INSTANCE_ATTRIBUTES={\"role\":\"${instance_role}\",\"host_index\":\"${host_index}\"}" >> /etc/ecs/ecs.config
+echo "ECS_ENABLE_TASK_IAM_ROLE_NETWORK_HOST=true" >> /etc/ecs/ecs.config
+echo "ECS_ENABLE_TASK_IAM_ROLE=true" >> /etc/ecs/ecs.config
+echo "ECS_AVAILABLE_LOGGING_DRIVERS=[\"json-file\",\"awslogs\",\"none\"]" >> /etc/ecs/ecs.config
 
 # Start/Restart ECS Agent
 systemctl enable --now ecs
